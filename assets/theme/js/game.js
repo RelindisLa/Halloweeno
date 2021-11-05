ce2406305ac4c0d9dd76d69c79944f37f3f3a318
Heidemarie Kofler <heidemarie.kofler@edu.campus02.at>
2021-11-05 09:41:39 +0100

    modaler dialog jetzt erst recht

--- assets/theme/js/game.js
+++ assets/theme/js/game.js
@@ -1,11 +1,11 @@
 let myModal = new bootstrap.Modal(document.getElementById('playerNames'));
 myModal.show();
 
-document.getElementById('playerNamesForm').addEventListener('keyup', function () {
+document.getElementById('playerNamesForm').addEventListener('keyup', function (evt) {
     console.log(evt);
 })
 
-document.getElementById('playerNamesForm').addEventListener('submit', function () {
+document.getElementById('playerNamesForm').addEventListener('submit', function (evt) {
     console.log("submit");
     evt.preventDefault();
     myModal.hide();
