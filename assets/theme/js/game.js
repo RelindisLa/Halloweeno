7ac96abf7682c325e824f409c4679979a0f5b435
Cinda <cindarella0@hotmail.com>
2021-11-26 08:36:33 +0100

    Karte heben inkl. Spielerinfo

--- assets/theme/js/game.js
+++ assets/theme/js/game.js
@@ -1,16 +1,22 @@
 const baseUrl = "assets/images/card/";
-let aktiverSpieler = 0;
-let playerliste = 0;
+let aktiverSpieler = [];
+let playerliste = [];
+let spielID = 0;
+let sp1 = {};
+let sp2 = {};
+let sp3 = {};
+let sp4 = {};
 
 let myModal = new bootstrap.Modal(document.getElementById('playerNames'));
 myModal.show();
 
 document.getElementById('playerNamesForm').addEventListener('keyup', function (evt) {
     //Namensunterscheidung
-    let player1 = document.getElementById('playerName1input').value;
-    let player2 = document.getElementById('playerName2input').value;
-    let player3 = document.getElementById('playerName3input').value;
-    let player4 = document.getElementById('playerName4input').value;
+    let player1 = document.getElementById('playerName1input').value.toUpperCase();
+    let player2 = document.getElementById('playerName2input').value.toUpperCase();
+    let player3 = document.getElementById('playerName3input').value.toUpperCase();
+    let player4 = document.getElementById('playerName4input').value.toUpperCase();
+    playerliste = [player1,player2,player3,player4];
 
     if (player1 == "" || player2 == "" || player3 == "" || player4 == "") {
         document.getElementById('playerNamesSubmit').disabled = true;
@@ -25,14 +31,8 @@ document.getElementById('playerNamesForm').addEventListener('submit', function (
     myModal.hide();
 })
 
-async function startGame() {
-    let player1 = document.getElementById('playerName1input').value.toUpperCase();
-    let player2 = document.getElementById('playerName2input').value.toUpperCase();
-    let player3 = document.getElementById('playerName3input').value.toUpperCase();
-    let player4 = document.getElementById('playerName4input').value.toUpperCase();
-    playerliste = [player1,player2,player3,player4];
-    //console.log("playerList ist: " + playerliste);
 
+async function startGame() {
     let response = await fetch("http://nowaunoweb.azurewebsites.net/api/Game/Start/", {
         method: 'POST',
         body: JSON.stringify(
@@ -41,57 +41,80 @@ async function startGame() {
         headers: {
             'Content-type': 'application/json; charset=UTF-8',
         }
-    })
-    let startinhalt;
-    if (response.ok) { // wenn http-status zwischen 200 und 299 liegt
-        startinhalt = await response.json(); // response Body auslesen
-        //console.log(startinhalt.stringify)
-        //alert(JSON.stringify(startinhalt));
-    }
-
-    //get and save SpielId:
-    const spielID = startinhalt.Id;
-
-    //Karten der Spieler positionieren:
-    let counter = 1;
-    startinhalt.Players.forEach(element => {
-        let wohin = "playerName" + counter;
-        let wo = document.getElementById(wohin);
-        wo.setAttribute("style", "text-align: center;");
-        wo.setAttribute("class", "row");
-        wo.setAttribute("style", "display: block");
-
-        let nameH4 = document.createElement("h4");
-        nameH4.setAttribute("style", "text-align: center");
-        let textName = document.createTextNode(element.Player);
-        wo.appendChild(nameH4);
-        nameH4.appendChild(textName);
-
-        let playerScore = document.createElement("p");
-        playerScore.setAttribute("style", "text-align: center; padding-top: 10px");
-        let textScore = document.createTextNode("Score: " + element.Score);
-        wo.appendChild(playerScore);
-        playerScore.appendChild(textScore);
-
-        let divA = document.createElement("div");
-        divA.setAttribute("id", "cardsOfPlayer");
-        wo.appendChild(divA);
-        element.Cards.forEach(el => {
-            const div = document.createElement("div");
-            div.setAttribute("style", "display: inline-block");
-            const img = document.createElement("img");
-            const card = `${el.Color}${el.Value}`;
-            img.src = `${baseUrl}${card}.png`;
-            img.setAttribute("class","rounded d-block");
-            img.setAttribute("style", "height: 80px; padding: 10px");
-            divA.appendChild(div);
-            div.appendChild(img);
-        })
-
-
-
-        counter++;
     });
+    if (response.ok) { // wenn http-status zwischen 200 und 299 liegt
+        let startinhalt = await response.json(); // response Body auslesen
+        erstPositionen(startinhalt);
+        spielID = saveSpielId(startinhalt);
+        spielID = startinhalt.Id;
+        console.log("Spielid im response " + spielID);
+        erstelltAblage(startinhalt);
+        erstelltHebestapel();
+        //AktiverSpieler:
+        startspieler(startinhalt);
+        aktiverSpieler = startinhalt.NextPlayer;
+        console.log("Startspieler im response: " + aktiverSpieler);
+        sp1 = startinhalt.Players[0];
+        sp2 = startinhalt.Players[1];
+        sp3 = startinhalt.Players[2];
+        sp4 = startinhalt.Players[3];
+    }
+}
+
+function startspieler(startinhalt){
+    aktiverSpieler = startinhalt.NextPlayer;
+}
+console.log("Startspieler ausserhalb response  " + aktiverSpieler);
+
+function saveSpielId(startinhalt){
+    let responseID = startinhalt.Id;
+    return responseID;
+}
+console.log("Spielid ausserhalb response " + spielID);
+
+
+function erstPositionen(startinhalt){
+ //Karten der Spieler positionieren:
+ let counter = 1;
+ startinhalt.Players.forEach(element => {
+     let wohin = "playerName" + counter;
+     let wo = document.getElementById(wohin);
+     wo.setAttribute("style", "text-align: center;");
+     wo.setAttribute("class", "row");
+     wo.setAttribute("style", "display: block");
+     wo.setAttribute("class", element.Player);
+
+     let nameH4 = document.createElement("h4");
+     nameH4.setAttribute("style", "text-align: center");
+     let textName = document.createTextNode(element.Player);
+     wo.appendChild(nameH4);
+     nameH4.appendChild(textName);
+
+     let playerScore = document.createElement("p");
+     playerScore.setAttribute("style", "text-align: center; padding-top: 10px");
+     let textScore = document.createTextNode("Score: " + element.Score);
+     wo.appendChild(playerScore);
+     playerScore.appendChild(textScore);
+
+     let divA = document.createElement("div");
+     divA.setAttribute("id", element.Player);
+     wo.appendChild(divA);
+     element.Cards.forEach(el => {
+         const div = document.createElement("div");
+         div.setAttribute("style", "display: inline-block");
+         const img = document.createElement("img");
+         const card = `${el.Color}${el.Value}`;
+         img.src = `${baseUrl}${card}.png`;
+         img.setAttribute("class","rounded d-block");
+         img.setAttribute("style", "height: 80px; padding: 10px");
+         divA.appendChild(div);
+         div.appendChild(img);
+     })
+
+     counter++;
+ });
+}
+function erstelltAblage(startinhalt){
 //Ablagestapel:
     let wo1 = document.getElementById("ablagestapel");
     const div1 = document.createElement("div");
@@ -101,7 +124,9 @@ async function startGame() {
     img1.src = `${baseUrl}${ablageCard}.png`;
     wo1.appendChild(div1);
     div1.appendChild(img1);
+}
 
+function erstelltHebestapel(){
 //Hebestapel:
     let wo2 = document.getElementById("hebestapel");
     const div2 = document.createElement("div");
@@ -111,58 +136,26 @@ async function startGame() {
     img2.src = `${baseUrl}${hebeCard}.png`;
     wo2.appendChild(div2);
     div2.appendChild(img2);
-
-
-//AktiverSpieler:
-    aktiverSpieler = startinhalt.NextPlayer; //Sting mit Namen
-    console.log("AktivPlayer is: " + aktiverSpieler);
 }
 
-//Aktiver Spieler:
-blurUnactivPlayer();
-//focusActivPlayer(aktiverSpieler);
 
+console.log("Alle Spieler: " + sp1 + ", " + sp2 + ", " + sp3 + ", " + sp4);
+console.log("sp1.ll" + sp1.Player);
+console.log("spielid: " + spielID);
+
+
+
+//Aktiver Spieler:
+//blurUnactivPlayer();
+
+/*
 function blurUnactivPlayer() {
     document.getElementById('spielerkarten1u2').addEventListener('blur',(event) => {
         event.target.style.background = '';});
     document.getElementById('spielerkarten3u4').addEventListener('blur', (event) => {
         event.target.style.background = '';});
 }
-
-function focusActivPlayer(player){
-    let p1 = document.querySelector('#h4').value;
-    
-    console.log("p1 Array: " + p1);
-    console.log("activePlayer is: " + player);
-
-    for(let i = 0; i < p1.length; i++){
-        if(i == player){
-            console.log("textContent of p1[1]: " + i);
-            p1[i].addEventListener('focus', (event) => {
-                event.target.style.background = 'black';
-              });
-        }
-    }
-}
-
-/*
-    p1.forEach(element => {
-         if(element.textContent != aktiverSpieler){
-            element.addEventListener('blur',(event) => {
-             event.target.style.background = '';});
-    }
-    p1.forEach(element => element.textContent == aktiverSpieler) {
-        element.addEventListener('focus')
-   }
-    p1.forEach(function(pl){
-        if(pl.textContent == aktiverSpieler){
-            pl.addEventListener("focus")
-        } else { pl.addEventListener("blur")}
-    });
-    //document.getElementById("spielfeld").addEventListener("blur",function(ev){   })
-    */
-
-
+*/
 
 // Karte ziehen
 document.getElementById('hebestapel').addEventListener('click', drawCard);
@@ -171,11 +164,31 @@ async function drawCard() {
         method: 'PUT',
     });
     let newCard;
-    if (response.ok) { // wenn http-status zwischen 200 und 299 liegt
+    if (response.ok) { 
         newCard = await response.json(); // response Body auslesen
         console.log(newCard);
+        addCard(newCard.Player,newCard.Card);
+        aktiverSpieler = newCard.NextPlayer;
     }
 }
+
+function addCard(player, el){
+
+    let wo = document.getElementById(player.Player);
+    let divA = document.createElement("div");
+        const div = document.createElement("div");
+        div.setAttribute("style", "display: inline-block");
+        const img = document.createElement("img");
+        const card = `${el.Color}${el.Value}`;
+        img.src = `${baseUrl}${card}.png`;
+        img.setAttribute("class","rounded d-block");
+        img.setAttribute("style", "height: 80px; padding: 10px");
+        divA.appendChild(div);
+        div.appendChild(img);
+
+}
+
+
 
 /*function convertNumber(cardValue) {
     if (cardValue < 15)
